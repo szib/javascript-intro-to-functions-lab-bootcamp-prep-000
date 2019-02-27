e55538c6cef7dd7d2676db910f858f795f727b4c
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 13:23:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
+  if (string.toLowerCase() === string) {
     return `I can't hear you!`
   }
   
-  if (string.toUpperCase === string) {
+  if (string.toUpperCase() === string) {
     return 'YES INDEED!'
   }
 }
